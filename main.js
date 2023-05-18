608c528dfdfb38ee95d8103d215e4dc9e3e9df7c
BondarAndrew <107639998+BondarAndrew@users.noreply.github.com>
2023-05-18 23:04:26 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+const h1 = document.getElementsByTagName('h1');
+for (let a of h1) {
+    a.textContent = 'Hello this is h1!';
+    a.style.color = 'red';
+    a.style.backgroundColor = 'black';
+}
+
+const h2 = document.getElementsByTagName('h2');
+for (let b of h2) {
+    b.textContent = 'Hello this is h2!';
+    b.style.color = 'green';
+    b.style.backgroundColor = 'black';
+}
+
+const p = document.getElementsByTagName('p');
+for (let c of p) {
+    c.textContent = 'Hello this is p!';
+    c.style.color = 'yellow'
+    c.style.backgroundColor = 'black';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
